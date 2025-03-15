aedb027605602955fae09c52f046c8d245b612cd
LancerTendy <korsun.lexa@mail.ru>
2025-03-15 08:31:47 +0700

    Update ManagerSaveException.java

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-//Поместила исключение в отдельный пакет
+//Поместил исключение в отдельный пакет
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
